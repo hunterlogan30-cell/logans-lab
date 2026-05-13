78ef78793ebd7ef0df1351df46cac6d017af99fd
Logan O’Neill <loganoneill@n3-7-62.dhcp.drexel.edu>
2026-05-13 14:50:08 -0400

    Add state param to Whoop OAuth

--- api/whoop/auth.js
+++ api/whoop/auth.js
@@ -1,12 +1,16 @@
+import { randomBytes } from 'crypto';
+
 export default function handler(req, res) {
   const clientId = process.env.VITE_WHOOP_CLIENT_ID;
   const redirectUri = 'https://logans-lab.vercel.app/api/whoop/callback';
+  const state = randomBytes(16).toString('hex'); // 32 character random string
 
   const params = new URLSearchParams({
     client_id: clientId,
     redirect_uri: redirectUri,
     response_type: 'code',
     scope: 'read:recovery read:cycles read:sleep read:workout read:profile read:body_measurement',
+    state,
   });
 
   res.redirect(`https://api.prod.whoop.com/oauth/oauth2/auth?${params}`);
